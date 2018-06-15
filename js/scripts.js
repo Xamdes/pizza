a3a6d49fc1bbc57cb75baea1a593538a79316c65
Steven Colburn <stevencolburn@xamdes.com>
2018-06-15 12:23:45 -0700

    add on submit put size and toppings into new pizza object

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,36 @@
-var toppings = [["Peperoni",1],["Onions",1],["Black Olives",2], ["Green Peppers",2]];
-var size = [["Small",4],["Medium",7],["Large",10]];
+
+
 
 $(function()
 {
   $("#input-form").submit(function(event){
     event.preventDefault();
-    getSize();
+    var newPizza = new Pizza();
+
+    newPizza.size = getSize();
+    newPizza.toppings = getToppings();
+    console.log(newPizza);
 
   });
 });
-
-
 
 //Front End
 
 function getSize()
 {
-  console.log(size[parseInt($("input:radio[name=input-size]:checked").val())]);
+  var size = [["Small",4],["Medium",7],["Large",10]];
+  return (size[parseInt($("input:radio[name=input-size]:checked").val())]);
+}
+
+function getToppings()
+{
+  var toppings = [["Peperoni",1],["Onions",1],["Black Olives",2], ["Green Peppers",2]];
+  var selected = [];
+  $("input:checkbox[name=input-toppings]:checked").each(function()
+  {
+    selected.push(toppings[parseInt($(this).val())]);
+  });
+  return selected;
 }
 
 //Back End
